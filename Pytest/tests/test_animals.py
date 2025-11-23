c935c76f6c8d347f097d79b316ef9c94a4d3d669
Jeff Cassell <jeffscassell@protonmail.com>
2025-11-22 21:58:02 -0600

    feat: Add parameterization examples to Pytest

--- Pytest/tests/test_animals.py
+++ Pytest/tests/test_animals.py
@@ -1,6 +1,6 @@
-from packageName.animals import Cat, Dog
-
 import pytest
+
+from packageName.animals import Cat, Dog
 
 
 
@@ -14,19 +14,31 @@ def baseCat():
 def baseDog():
     return Dog()
 
-# a complex fixture calling other fixtures
+# A complex fixture calling other fixtures.
 @pytest.fixture
 def animals(baseDog, baseCat):
     return [baseDog, baseCat]
 
+@pytest.fixture(params=(0,1,2))
+def multiple(request):
+    return request.param
+
 ### TESTS ###
 
-# calls a simple fixture
-def test_catMeows(baseCat):
+# Calls a simple fixture.
+def test_cat_meows(baseCat):
     assert baseCat.speak() == "meow"
-    
-# calls a more complex fixture
-def test_animalsSpeak(animals):
+
+# Calls a slightly more complex fixture.
+def test_animals_speak(animals):
     for animal in animals:
         assert animal.speak() in ["meow", "bark"]
 
+def test_raises_exception():
+    with pytest.raises(OSError):
+        raise OSError
+
+# Creates 3 tests by itself due to the `multiple` fixture's multiple calls.
+def test_multiple_calls(multiple):
+    assert multiple in (0,1,2)
+    # assert 0  # Will always fail the test, but allows us to see the value.
